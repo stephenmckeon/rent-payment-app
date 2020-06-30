62aea42cebca6974c05eafddc177e437936540a0
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-06-30 09:47:32 -0400

    Add edit and new functionality to Platform

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -1,8 +1,31 @@
 class PlatformsController < ApplicationController
-  before_action :set_platform, only: %i[show]
+  before_action :set_platform, only: %i[show edit update]
 
   def index
     @platforms = Platform.all
+  end
+
+  def new
+    @platform = Platform.new
+  end
+
+  def create
+    @platform = Platform.new(platform_params)
+    if @platform.save
+      redirect_to platform_path(@platform)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @release_date = @platform.release_date.to_date
+  end
+
+  def update
+    @platform.update(platform_params)
+
+    redirect_to platform_path(@platform)
   end
 
 private
--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,4 +1,8 @@
 class Platform < ApplicationRecord
   has_many :video_games
   has_many :gamers, through: :video_games
+
+  def readable_release_date
+    release_date.to_date.strftime("%B %d, %Y")
+  end
 end
