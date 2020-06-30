a904fc4ee05e54d3a12c20d9dcd2785b14591c56
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-06-30 09:51:23 -0400

    Add validations to Platform

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -2,6 +2,9 @@ class Platform < ApplicationRecord
   has_many :video_games
   has_many :gamers, through: :video_games
 
+  validates :name, :developer, :release_date, presence: true
+  validates_uniqueness_of :name
+
   def readable_release_date
     release_date.to_date.strftime("%B %d, %Y")
   end
