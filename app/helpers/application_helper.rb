1cb890404fa21239b2161987e0bb9d2222291034
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-07-01 14:53:22 -0400

    Add nav bar for logged out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
     if session[:gamer_id].blank?
       render "header_logged_out"
     else
+      @gamer = Gamer.find(session[:gamer_id])
       render "header_logged_in"
     end
   end
